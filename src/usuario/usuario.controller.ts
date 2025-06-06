3ab13009abef3dfd04eb6fd1a0538c6e216e234e
Marcio Vieira <marciovds07@gmail.com>
2025-06-06 20:37:46 -0300

    fix - atualizando controller com UpdateDTO

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -23,7 +23,7 @@ export class UsuarioController {
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUsuarioDto: CreateUsuarioDto) {
+  async update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     try {
       const result = await this.usuarioService.update(+id, updateUsuarioDto);
       return { status: 'successo', message: result.message, data: result.data };
