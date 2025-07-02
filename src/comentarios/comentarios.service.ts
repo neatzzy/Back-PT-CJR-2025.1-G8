6a18d548cf52c3bee666ab74eef1bc2df712c0d3
VitorGuedes22 <studyprofessional18@gmail.com>
2025-07-02 18:32:54 -0300

    hotfix : removendo transação do create de comentario para não dar tle

--- src/comentarios/comentarios.service.ts
+++ src/comentarios/comentarios.service.ts
@@ -13,51 +13,53 @@ export class ComentariosService {
   
   async create(createComentarioDto: CreateComentarioDto) {
     const { userId, avaliacaoId, conteudo } = createComentarioDto;
+
     const usuario = await this.prisma.usuario.findUnique({ where: { id: userId } });
-        if (!usuario) {
-          throw new NotFoundException('Usuário não encontrado.');
-        }
+
+    if (!usuario) {
+      throw new NotFoundException('Usuário não encontrado.');
+    }
+
     try{
-      return await this.prisma.$transaction(async (tx) => {
-        // Verifica se a avaliação existe
-      const relacao = await this.prisma.avaliacao.findUnique({
-        where: { id: avaliacaoId },
-      });
-      if (!relacao) {
-        throw new NotFoundException('Avaliação não encontrada.');
-      }
-      const dataComentario: Prisma.ComentariosCreateInput = {
-        conteudo,
-        usuario: { connect: { id: userId } },
-        avaliacao: { connect: { id: avaliacaoId } },
-      };
-
-      return this.prisma.comentarios.create({ data: dataComentario });
-    });
-
-  } catch (error) {
-    if (error instanceof PrismaClientKnownRequestError) {
-        
-      // Erro de constraint de chave estrangeira
-      if (error.code === 'P2003') {
-        throw new BadRequestException({
-          message: 'Violação de integridade referencial. Verifique se todos os IDs referenciados existem.',
-          prismaError: error.meta,
-        });
-      }
-        
-        // Outros erros conhecidos do Prisma
-        throw new BadRequestException({
-          message: 'Erro Prisma: ' + error.message,
-          prismaError: error.meta,
-        });
-      }
       
-      handlePrismaError(error);
-    }    
-    
-    //return 'Essa ação cria um novo comentario';
-  }
+        const relacao = await this.prisma.avaliacao.findUnique({
+          where: { id: avaliacaoId },
+        });
+        if (!relacao) {
+          throw new NotFoundException('Avaliação não encontrada.');
+        }
+
+        const dataComentario: Prisma.ComentariosCreateInput = {
+          conteudo,
+          usuario: { connect: { id: userId } },
+          avaliacao: { connect: { id: avaliacaoId } },
+        };
+
+        return await this.prisma.comentarios.create({ data: dataComentario });
+
+      } catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+            
+          // Erro de constraint de chave estrangeira
+          if (error.code === 'P2003') {
+            throw new BadRequestException({
+              message: 'Violação de integridade referencial. Verifique se todos os IDs referenciados existem.',
+              prismaError: error.meta,
+            });
+          }
+            
+            // Outros erros conhecidos do Prisma
+            throw new BadRequestException({
+              message: 'Erro Prisma: ' + error.message,
+              prismaError: error.meta,
+            });
+          }
+          
+          handlePrismaError(error);
+        }    
+      
+      //return 'Essa ação cria um novo comentario';
+    }
 
   async findAll() {
     return `Essa ação retorna todos os comentarios`;
